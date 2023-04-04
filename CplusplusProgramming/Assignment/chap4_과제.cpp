83e4df068a55ebf86fe29b12fbabad425b37e29b
ByeongHyeon Kim <bhkim0404@gmail.com>
2023-04-05 00:29:44 +0900

    how_to_return_class?

--- CplusplusProgramming/Assignment/chap4_과제.cpp
+++ CplusplusProgramming/Assignment/chap4_과제.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
 //º¹¼Ò¼ö¿¡ ´ëÇÑ »çÄ¢¿¬»êÀÌ °¡´ÉÇÏµµ·Ï ±¸ÇöÇÑ´Ù. main() ÇÔ¼ö´Â ¼öÁ¤ÇÏ¸é ¾ÈµÈ´Ù ±¸±Û¸µÇØº¸ÀÚ~
 using namespace std;
+
 class ComplexNumber {
 private:
 	float real; //½Ç¼ö
 	float imaginary; //Çã¼ö
 public:
-	//ÄÚµå
-	
 	ComplexNumber(float real, float imaginary): real(real), imaginary(imaginary) { //instead of this->real = real; 
 		//this->real = real;
 		//this->imaginary = imaginary;
 	}
-	//ComplexNumber& operator+(const ComplexNumber&);
-	friend ComplexNumber& operator+(const ComplexNumber&, const ComplexNumber&);
-	ComplexNumber add(const ComplexNumber);
-
+	ComplexNumber& operator+(const ComplexNumber&);
+	friend ComplexNumber& operator+(const ComplexNumber&, const ComplexNumber&); //operator_overloading
 	friend ostream& operator<<(ostream& stream, const ComplexNumber& p);
+	ComplexNumber& add(const ComplexNumber&);
 
 	
 };
-ComplexNumber& operator+(const ComplexNumber&a, const ComplexNumber&b) {
-	a.real += b.real;
-	a.imaginary += b.imaginary;
+// ComplexNumber& operator+(const ComplexNumber&a, const ComplexNumber&b) { //operator_overloading
+// 	a.real += b.real;
+// 	a.imaginary += b.imaginary;
 
-	return a;
-	//return *a;
-	//return &a;
-}
+// 	return a;
+// 	//return *a;
+// 	//return &a;
+// }
 
 ostream& operator<<(ostream& stream, const ComplexNumber& p) {
 	stream << p.real << " + " << p.imaginary << "i";
 	return stream;
 }
 
-//ComplexNumber add(const ComplexNumber &b) {} ÀÌ·¨À» ¶§ ¾È µÇ´Â °Å È®ÀÎ
-ComplexNumber& ComplexNumber::add(const ComplexNumber &b) {
-	ComplexNumber cn;
+//ComplexNumber add(const ComplexNumber &b) {} //ÀÌ·¨À» ¶§ ¾È µÇ´Â °Å È®ÀÎ
+ComplexNumber& ComplexNumber::add(const ComplexNumber &b) { //how to return class?
+	ComplexNumber cn(0, 0);
+
+	cn.real = this->real + b.real;
+	cn.imaginary = this->imaginary + b.imaginary;
 
 	return cn;                        
 }
@@ -52,36 +53,37 @@ int main(void) {//mainÀº ¼öÁ¤ÇÒ °Í ¾øÀ½
 		case 1:
 			cout << endl << "µ¡¼À d = a + b + c" << endl;
 			cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
+			cout << a << endl;
 			d = a.add(b.add(c));
 			cout << "d = " << d << endl;
 			break;
-		case 2:
-			cout << endl << "»¬¼À d = a - b - c" << endl;
-			cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
-			d = a.subtract(b.subtract(c));
-			cout << "d = " << d << endl;
-			break;
+		// case 2:
+		// 	cout << endl << "»¬¼À d = a - b - c" << endl;
+		// 	cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
+		// 	d = a.subtract(b.subtract(c));
+		// 	cout << "d = " << d << endl;
+		// 	break;
 
-		case 3:
-			cout << endl << "°ö¼À d = a * b * c" << endl;
-			cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
-			d = a.multiply(b.multiply(c));
-			cout << "d = " << d << endl;
-			//e = a - b - c - d;
-			break;
-		case 4:
-			cout << endl << "³ª´°¼À d = a / b / c" << endl;
-			cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
-			d = a.divide(b.divide(c));
-			cout << "d = " << d << endl;
-			cout << a;
-			break;
-		case 5:
-			cout << endl << "cascading µ¡¼À d = a + b + c" << endl;
-			cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
-			d = a + b + c;//operator_overloading
-			cout << "d = " << d << endl;
-			break;
+		// case 3:
+		// 	cout << endl << "°ö¼À d = a * b * c" << endl;
+		// 	cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
+		// 	d = a.multiply(b.multiply(c));
+		// 	cout << "d = " << d << endl;
+		// 	//e = a - b - c - d;
+		// 	break;
+		// case 4:
+		// 	cout << endl << "³ª´°¼À d = a / b / c" << endl;
+		// 	cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
+		// 	d = a.divide(b.divide(c));
+		// 	cout << "d = " << d << endl;
+		// 	cout << a;
+		// 	break;
+		// case 5:
+		// 	cout << endl << "cascading µ¡¼À d = a + b + c" << endl;
+		// 	cout << endl << "a = " << a << ", b = " << b << ", c = " << c << endl;
+		// 	d = a + b + c;//operator_overloading
+		// 	cout << "d = " << d << endl;
+		// 	break;
 		default:
 			exit(0);
 			break;
